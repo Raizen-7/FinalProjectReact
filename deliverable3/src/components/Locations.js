f038b1275f818b363137c68ab246c159b400c2ff
Raizen-7 <ferminyanes07@gmail.com>
2022-05-14 23:44:15 -0500

    Start layout with CSS

--- deliverable3/src/components/Locations.js
+++ deliverable3/src/components/Locations.js
@@ -8,23 +8,23 @@ const Locations = () => {
 
     const [id, setId] = useState("");
 
-    
+
     //Hook controlador de bucles dependiendo de las dependencias incertadas en el arreglo.
-    useEffect(()=>{
+    useEffect(() => {
         // Contante que guarda los ID consultados de forma aleatorioa desde la API
-        const random = Math.floor(Math.random() * 126 ) + 1;
+        const random = Math.floor(Math.random() * 126) + 1;
         //
-        axios.get(`https://rickandmortyapi.com/api/location/${random}/`).then((res)=> setLocation(res.data));
-    },[]);
+        axios.get(`https://rickandmortyapi.com/api/location/${random}/`).then((res) => setLocation(res.data));
+    }, []);
 
     // console.log(locations);
     // console.log(id)
 
-    const searchLocations = () =>{
-       
-        if( id <= 126 ){
-        axios.get(`https://rickandmortyapi.com/api/location/${id}/`).then((res)=> setLocation(res.data));
-        }else{
+    const searchLocations = () => {
+
+        if (id <= 126) {
+            axios.get(`https://rickandmortyapi.com/api/location/${id}/`).then((res) => setLocation(res.data));
+        } else {
             alert("This only have 126 id to show")
         }
 
@@ -32,28 +32,46 @@ const Locations = () => {
 
 
     return (
-        <div>
-            <h1>{locations.name}</h1>
-            <div className='This'>
-            <p>Type: {locations.type}</p>
-            <p>Dimension : {locations.dimension}</p>
-            <p>Population: {locations.residents?.length}</p>
-            </div>
-            <div>
-                <input  type="number"
-                        onChange={(e) => setId(e.target.value)} 
-                        value={id} 
-                />
-                <button onClick={searchLocations}>Search</button>
-                <ul>
-                    {locations.residents?.map(resident => (
-                       <Residents url={resident} key={resident} />
-                    ))}
+        <main>
+            <h1 className='locations'>{locations.name}</h1>
 
-                </ul>
-                
-            </div>
-        </div>
+            <div className='content'>
+
+                <div className='contentThis'>
+                    <div className='This'>
+                        <div className='pepito'>
+                        <p>Type: {locations.type}</p>
+                        <p>Dimension : {locations.dimension}</p>
+                        <p>Population: {locations.residents?.length}</p>
+                        </div>
+                        <div className='contentInputAndButton'>
+                            <div className='contentInput'>
+                                <input type="number"
+                                    onChange={(e) => setId(e.target.value)}
+                                    value={id} />
+                                <button onClick={searchLocations}>Search</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+                </div>
+
+                <div>
+
+                    <ul>
+                        {locations.residents?.map(resident => (
+                            <Residents url={resident} key={resident} />
+                        ))}
+                    </ul>
+                </div>
+
+            
+
+        </main>
     );
 };
 
